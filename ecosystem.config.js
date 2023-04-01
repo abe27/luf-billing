de86bc02de39818b66c3f6983810ba425fbed3df
Taweechai Yuenyang <krumii.it@gmail.com>
2023-04-01 16:54:25 +0700

    add ecgosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: "next",
+      cwd: "./",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
